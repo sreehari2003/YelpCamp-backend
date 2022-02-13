0d8803eb633584ed534ef036a76217174a2258eb
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-02-14 01:32:29 +0530

    added review controller

--- routes/camp.js
+++ routes/camp.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.route("/camp").get(campControl.getAllCamps).post(campControl.createCamp);
 router.route("/camp/:id").get(campControl.getOneCamp);
+router.route("/camp/:id/reviews").post(campControl.createReview);
 
 module.exports = router;
